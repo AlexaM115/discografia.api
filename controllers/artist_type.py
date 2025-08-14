44065f09e037bd1732a3a2b75f53c0961ee005d9
AlexaM115 <amymartinez2005mr@gmail.com>
2025-08-14 12:20:04 -0600

    Actualización

--- controllers/artist_type.py
+++ controllers/artist_type.py
@@ -3,6 +3,11 @@ from utils.mongodb import get_collection
 from fastapi import HTTPException
 from bson import ObjectId
 
+from pipelines.artist_type_pipeline import (
+    get_artist_type_pipeline
+    , validate_type_is_assigned_pipeline
+)
+
 collection = get_collection("artist_types")
 
 def create_artist_type(tipo: ArtistType):
@@ -13,6 +18,15 @@ def create_artist_type(tipo: ArtistType):
 
 def get_all_artist_types():
     return [{**doc, "_id": str(doc["_id"])} for doc in collection.find()]
+
+async def get_artist_types() -> list:
+    try:
+        pipeline = get_artist_type_pipeline()
+        catalog_types = list(collection.aggregate(pipeline))
+        return catalog_types
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error fetching catalog types: {str(e)}")
+
 
 def get_artist_type(id: str):
     tipo = collection.find_one({"_id": ObjectId(id)})
@@ -38,3 +52,24 @@ def delete_artist_type(id: str):
     if result.deleted_count == 0:
         raise HTTPException(status_code=404, detail="Tipo no encontrado")
     return {"message": "Tipo de artista eliminado"}
+
+def deactivate_artist_type(artist_type_id: str) -> dict:
+    try:
+        pipeline = validate_type_is_assigned_pipeline(artist_type_id)
+        assigned = list(collection.aggregate(pipeline))
+
+        if assigned is None:
+            raise HTTPException(status_code=404, detail="Artist type not found")
+
+        if assigned[0]["number_of_products"] > 0:
+            collection.update_one(
+                {"_id": ObjectId(artist_type_id)},
+                {"$set": {"active": False}}
+            )
+            return {"message": "Artist type is assigned to products and has been deactivated"}
+        else:
+            collection.delete_one({"_id": ObjectId(artist_type_id)})
+            return {"message": "Artist type deleted successfully"}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error deactivating artist type: {str(e)}")
