d68e3168c5420f99efc912c989a7e94dd543fb33
AlexaM115 <amymartinez2005mr@gmail.com>
2025-08-14 13:05:39 -0600

    Actualización

--- main.py
+++ main.py
@@ -27,15 +27,6 @@ from routes.songs_by_filter import router as songs_by_filter_router
 
 app = FastAPI()
 
-from fastapi.middleware.cors import CORSMiddleware
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins for development; restrict in production
-    allow_credentials=True,
-    allow_methods=["*"],  # Allow all methods
-    allow_headers=["*"],  # Allow all headers
-)
-
 
 from fastapi.openapi.utils import get_openapi
 def custom_openapi():
@@ -63,6 +54,16 @@ def custom_openapi():
     return app.openapi_schema
 
 app.openapi = custom_openapi
+
+
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins for development; restrict in production
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 
 app.include_router(artist_router)
